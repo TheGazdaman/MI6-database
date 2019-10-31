f35143b84cbbc2342d9fe42e7978cf7176f42517
Paul Coltofeanu <paulcolto@gmail.com>
2019-10-31 11:25:14 +0100

    relationships

---
+++ app/Person.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Person extends Model
+{
+    public function aliases() {
+        return $this->hasMany('App/Alias');
+    }
+
+    public function image() {
+        return $this->hasOne('App/Image');
+    }
+
+    public function status() {
+        return $this->hasOne('App/Status');
+    }
+}
